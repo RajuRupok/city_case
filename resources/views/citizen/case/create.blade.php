55fcae74b6b30a92fe471be9b94c779be2312826
nikhil.lu210 <nikhil.lu210@gmail.com>
2021-08-20 03:21:36 +0600

    Create Case UI

--- resources/views/citizen/case/create.blade.php
+++ resources/views/citizen/case/create.blade.php
@@ -4,12 +4,28 @@
 
 @section('stylesheet_links')
     {{--  External CSS  --}}
-
+    <link rel="stylesheet" href="{{ asset('frontend/assets/css/summernote-bs4.css') }}">
 @endsection
 
 @section('stylesheet')
     {{--  External CSS  --}}
-
+    <style>
+        select, textarea {
+            outline: none !important;
+            box-shadow: none !important;
+            border-radius: 0px !important;
+            border: 1px solid var(--primaryColor) !important;
+        }
+        .modal-backdrop {
+            position: absolute;
+            top: 0;
+            left: 0;
+            z-index: 1040;
+            width: auto;
+            height: auto;
+            background-color: #000;
+        }
+    </style>
 @endsection
 
 @section('content')
@@ -17,17 +33,58 @@
 <section id="myCases" class="features my-cases mt-5" style="min-height: 90vh;">
     <div class="container">
         <div class="section-title" data-aos="fade-up">
-            <h2>{{ __('My All Cases') }}</h2>
-            {{-- <p>{{ __('All services that are provided by Us') }}</p> --}}
+            <h2>{{ __('Create New Case') }}</h2>
         </div>
 
-        <div class="row" data-aos="fade-up" data-aos-delay="300">
-            <div class="col-md-4 mb-4">
-                <div class="icon-box">
-                    <i class="ri-gradienter-line" style="color: var(--primaryColor);"></i>
-                    <h3>
-                        <a href="{{ route('citizen.case.show', ['case_id' => decbin(1)]) }}">{{ 'Case Full Title Here' }}</a>
-                    </h3>
+        <div class="row justify-content-center" data-aos="fade-up" data-aos-delay="300">
+            <div class="col-md-12">
+                <div class="card">
+                    <form action="#" method="post" enctype="multipart/form-data">
+                        @csrf
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-6 mb-3">
+                                    <label for="category">Case Category <sup class="text-danger">*</sup></label>
+                                    <select name="category" class="form-select @error('category') is-invalid @enderror" required>
+                                        <option selected>Select Category</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </select>
+        
+                                    @error('category')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+
+                                <div class="col-md-6">
+                                    <label for="title">Case Title <sup class="text-danger">*</sup></label>
+                                    <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" value="{{ old('title') }}" required placeholder="Case Title">
+        
+                                    @error('title')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+
+                                <div class="col-md-12">
+                                    <label for="details">Case Description <sup class="text-danger">*</sup></label>
+                                    <textarea id="details" name="details" class="form-control @error('details') is-invalid @enderror" placeholder="Details" required rows="4">{{ old('details') }}</textarea>
+        
+                                    @error('details')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <button class="btn btn-dark btn-custom float-end mb-3 mt-3">{{ __('Submit Case') }}</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
@@ -39,10 +96,29 @@
 
 @section('script_links')
     {{--  External Javascript  --}}
-
+    <script src="{{ asset('frontend/assets/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/summernote-bs4.min.js') }}"></script>
 @endsection
 
 @section('scripts')
     {{--  External Javascript  --}}
-
+    <script>
+        "use strict";
+        $(document).ready(function() {
+            /* -- Form Editors - Summernote -- */
+            $('#details').summernote({
+                placeholder: 'Write your Case here...',
+                height: 320,
+                minHeight: null,
+                maxHeight: null,
+                focus: true,
+                toolbar: [
+                    ['font', ['bold', 'underline']],
+                    ['para', ['ul', 'ol']],
+                    ['insert', ['link', 'picture']],
+                    ['view', ['fullscreen', 'help']]
+                ]
+            });
+        });
+    </script>
 @endsection
