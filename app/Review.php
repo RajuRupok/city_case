8dd57dfdf7a30eae41b523cf66b2b4463fd10e72
nikhil.lu210 <nikhil.lu210@gmail.com>
2021-08-20 21:23:44 +0600

    Reletions

--- app/Review.php
+++ app/Review.php
@@ -9,12 +9,12 @@ class Review extends Model
     //relation with reviewer many to one relationship
     public function reviewer()
     {
-        return $this->hasOne('App\User', 'reviewer_id', 'id');
+        return $this->belongsTo('App\User', 'reviewer_id', 'id');
     }
     
     //relation with case many to one relationship
     public function case()
     {
-        return $this->hasOne('App\CityCase', 'case_id', 'id');
+        return $this->belongsTo('App\CityCase', 'case_id', 'id');
     }
 }
